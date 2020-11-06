0d4db2471c2dd4d5b7411bd8efb1039c4dec8ef0
Micah Rufsvold <mjrufsvold@protonmail.com>
2020-11-05 22:58:25 -0500

    final FLB edits for the night

--- FLB Reporter/FLB Report Tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestReportTools(unittest.TestCase):
-
-    def test_find_logs(self):
-        from main.py import get_files_from_dir
-        self.assertGreater(len(get_files_from_dir('Logs')),0)
-
-
